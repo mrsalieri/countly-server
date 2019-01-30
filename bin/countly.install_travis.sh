d8c943dfcd95dd96264cb83957a3c725c97812cc
Arturs Sosins <ar2rsawseen@gmail.com>
2019-01-30 10:06:13 +0200

    _

--- bin/countly.install_travis.sh
+++ bin/countly.install_travis.sh
@@ -11,6 +11,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 bash $DIR/scripts/logo.sh;
 
+#make swap file
+#bash $DIR/scripts/make.swap.sh
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
 #update package index
