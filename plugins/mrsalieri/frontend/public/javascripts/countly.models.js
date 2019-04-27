6fa3444796cb61123ab957901895641f9a42cb29
msisman <msisman8054@gmail.com>
2019-04-27 18:13:19 +0300

    aggregation param added, date range arrays modified

--- plugins/mrsalieri/frontend/public/javascripts/countly.models.js
+++ plugins/mrsalieri/frontend/public/javascripts/countly.models.js
@@ -16,21 +16,38 @@
         // 60days 60days true
         // calendar multi day Array [ 1551484800000, 1556150400000 ] true
         // calendar single day [ 1555977600000, 1555977600000 ] false
+
         var periodObj = countlyCommon.getPeriodObj();
         var period = countlyCommon.getPeriod();
 
         var rangeAry = []; // date list data that being searched
+        var agg = 'day';
 
         if (['yesterday', 'hour'].includes(period)) { // today & yesterday
             rangeAry = [periodObj.activePeriod];
         }
         else if (period === 'month') { // year
-            rangeAry = []; // range array should be modified for period month
+            agg = 'month'; // aggregate in terms of months
+
+            // fill the range array with starting days of months
+            var year = moment().format('YYYY');
+            for (var x = 1; x <= 12; x += 1) {
+                rangeAry.push(year + '.' + x + '.1');
+            }
+        }
+        else if (period === 'day') { // month
+            var startDateOfMonth = moment().startOf('month');
+            var endDateOfMonth = moment().endOf('month');
+
+            // fill the range array with days of the month
+            for (var date = startDateOfMonth; date <= endDateOfMonth; date = date.add(1, 'days')) {
+                rangeAry.push(date.format('YYYY-M-D'));
+            }
         }
         else if (Array.isArray(period) && periodObj.numberOfDays === 1) { // calendar single day
             rangeAry = [moment(period[0]).format('YYYY-M-D')];
         }
-        else { // remainig options
+        else { // remaining options
             rangeAry = Array.isArray(periodObj.currentPeriodArr) ? periodObj.currentPeriodArr : [];
         }
 
@@ -43,8 +60,9 @@
             url: countlyCommon.API_URL + '/o/mrsalieri',
             data: {
                 app_id: countlyCommon.ACTIVE_APP_ID,
-                start_date: moment(startDate, 'YYYY-M-D').format('YYYY-MM-DD'),
-                end_date: moment(endDate, 'YYYY-M-D').format('YYYY-MM-DD'),
+                start_date: moment(startDate, ['YYYY-M-D', 'YYYY-M']).format('YYYY-MM-DD'),
+                end_date: moment(endDate, ['YYYY-M-D', 'YYYY-M']).format('YYYY-MM-DD'),
+                agg: agg,
             },
             success: function(response) {
                 _data = {
